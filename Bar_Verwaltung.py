5e925c94c956a8b0850b6690ea54f63707184e43
The_Omega_Rexx <71725528+TheOmegaRexx@users.noreply.github.com>
2023-11-30 09:32:12 +0100

    Add files via upload

--- Bar_Verwaltung.py
+++ Bar_Verwaltung.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from CTkMessagebox import CTkMessagebox
 import mysql.connector
 import customtkinter
 from CTkListbox import *
@@ -40,9 +41,10 @@ class GUI:
         self.statistiken_button.grid(row=4, column=0, padx=5, pady=5, sticky=E)
 
         self.fenster.mainloop()
+        
 
     def tressen_oeffnen(self):
-        bar = Tressen()
+        tressen = Tressen()
 
     def lager_oeffnen(self):
         lager = Lager()
@@ -55,7 +57,75 @@ class GUI:
 
 
 class Tressen():
-    pass
+        def __init__(self):
+            preis = 0
+            self.tressen_fenster = customtkinter.CTk()
+            self.tressen_fenster.title("Tressen")
+            self.tressen_fenster.geometry()
+            self.tressen_fenster.resizable(False, False)
+
+            self.tressen_label = customtkinter.CTkLabel(
+                self.tressen_fenster, text="Tressen")
+            self.tressen_label.grid(row=0, column=0)
+
+            self.auswahl_label = customtkinter.CTkLabel(
+                self.tressen_fenster, text="Artikel")
+            self.auswahl_label.grid(row=1, column=0)
+
+            self.bestellung_label = customtkinter.CTkLabel(
+                self.tressen_fenster, text="Bestellung")
+            self.bestellung_label.grid(row=1, column=1)
+
+            self.preis_label = customtkinter.CTkLabel(self.tressen_fenster, text=f"Preis: {preis}")
+            self.preis_label.grid(row=0, column=1)
+
+            self.auswahl_listbox = CTkListbox(self.tressen_fenster)
+            self.auswahl_listbox.grid(row=2, column=0)
+
+            self.bestellung_listbox = CTkListbox(self.tressen_fenster)
+            self.bestellung_listbox.grid(row=2, column = 1)
+
+            self.hinzufuegen_button = customtkinter.CTkButton(self.tressen_fenster, text="Hinzufügen", command=self.hinzufuegen)
+            self.hinzufuegen_button.grid(row=3, column=0)
+
+            self.entfernen_button = customtkinter.CTkButton(self.tressen_fenster, text="Entfernen", command=self.entfernen)
+            self.entfernen_button.grid(row=3, column=1)
+
+            self.bestellung_button = customtkinter.CTkButton(self.tressen_fenster, text="Bestellen", command=self.bestellung)
+            self.bestellung_button.grid(row=4, column=1)
+
+            self.nachricht_button = customtkinter.CTkButton(self.tressen_fenster, text="Nachricht", command=self.nachricht)
+            self.nachricht_button.grid(row=4, column=0)
+
+
+            self.tressen_fenster.mainloop()
+
+
+        def bestellung(self):
+            pass
+
+        def hinzufuegen(self):
+            pass
+
+        def entfernen(self):
+            pass
+
+        def nachricht(self):
+            nachrichten_fenster = customtkinter.CTkToplevel(self.tressen_fenster)
+            nachrichten_label = customtkinter.CTkLabel(nachrichten_fenster, text="Nachricht").pack()
+            nachrichten_textbox = customtkinter.CTkTextbox(nachrichten_fenster).pack()
+            nachrichten_button = customtkinter.CTkButton(nachrichten_fenster, text="Senden", command=self.nachricht_senden).pack()
+
+            nachrichten_fenster.mainloop
+
+        def nachricht_senden(self):
+            auswahl = CTkMessagebox(title="Exit?", message="Möchten sie die Nachricht senden?",
+                        icon="question", option_1="Nein", option_2="Ja")
+            
+            wahl = auswahl.get()
+            if wahl == "Ja":
+                CTkMessagebox(title="Erfolg", message="Die Nachricht wurde Erfolgreich gesendet", icon="check")
+
 
 
 class Lager:
@@ -72,7 +142,7 @@ class Lager:
         self.artikel_listbox = CTkListbox(self.lager_fenster)
         self.artikel_listbox.grid(row=1)
 
-        self.artikel_listbox.insert(0, "Option 0")
+        self.artikel_listbox.insert(0, "Name 0 3$")
 
         self.artikel_hinzufuegen_button = customtkinter.CTkButton(
             self.lager_fenster, text="Hinzufügen", command=self.artikel_hinzufuegen)
@@ -103,13 +173,16 @@ class Rechnungen():
         self.rechnungen_label.grid(row=0, column=0)
 
         self.rechnungs_listbox = CTkListbox(self.rechnungs_fenster)
-        self.rechnungs_listbox.grid(row=1)
+        self.rechnungs_listbox.grid(row=1, column=0)
 
         self.rechnungs_listbox.insert(0, "Option 0")
 
+        self.rechnungs_textbox = customtkinter.CTkTextbox(self.rechnungs_fenster)
+        self.rechnungs_textbox.grid(row=1, column=1)
+
         self.speichern_button = customtkinter.CTkButton(
             self.rechnungs_fenster, text="Speichern", command=self.speichern)
-        self.speichern_button.grid(row=0, column=1)
+        self.speichern_button.grid(row=3, column=0)
 
         self.rechnungs_fenster.mainloop()
 
@@ -124,21 +197,21 @@ class Statistik():
         self.statistik_fenster.geometry()
         self.statistik_fenster.resizable(False, False)
 
+        self.geld_label = customtkinter.CTkLabel(
+            self.statistik_fenster, text=f"Geld: {geld}")
+        self.geld_label.grid(row=0, column=0)
+
         self.profit_label = customtkinter.CTkLabel(
             self.statistik_fenster, text="Profit: ")
-        self.profit_label.grid(row=0, column=0)
-
-        self.verkauf_label = customtkinter.CTkLabel(
-            self.statistik_fenster, text="Anzahl der Verkäufe: ")
-        self.verkauf_label.grid(row=1, column=0)
+        self.profit_label.grid(row=1, column=0)
 
         self.belibste_label = customtkinter.CTkLabel(
             self.statistik_fenster, text="Beliebstes Artikel: ")
         self.belibste_label.grid(row=2, column=0)
 
-        self.titel1_label = customtkinter.CTkLabel(
-            self.statistik_fenster, text="WIP:")
-        self.titel1_label.grid(row=3, column=0)
+        self.verkauf_label = customtkinter.CTkLabel(
+            self.statistik_fenster, text="Anzahl der Verkäufe: ")
+        self.verkauf_label.grid(row=3, column=0)
 
         self.titel2_label = customtkinter.CTkLabel(
             self.statistik_fenster, text="WIP: ")
